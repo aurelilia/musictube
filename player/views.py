3ef3cdfaa0bddf4243ef1d73647f850f759c0ca0
Ellie Ang <git@angm.xyz>
2017-12-30 03:13:21 +0100

    HTML+Sass for navbar + player homepage view

--- player/views.py
+++ player/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """ Returns the main page. """
+    return render(request, 'player/index.html')
--- settings.py
+++ settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'musictube',
+    'musictube.player',
+    'musictube.manager'
 ]
 
 MIDDLEWARE = [
