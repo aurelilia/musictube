dc17a06ad81d1a271235dbdf06108a5b9a06c1b7
Ellie Ang <git@angm.xyz>
2017-12-31 17:02:49 +0100

    Added login+registration page; Playlist list view added

--- player/views.py
+++ player/views.py
@@ -6,6 +6,10 @@ from musictube.models import Playlist
 def home(request):
     """ Returns the main page. """
     if request.user.is_authenticated:
+        playlists = Playlist.objects.filter(user=request.user)
+        context = {'playlists': playlists}
+        for playlist in playlists:
+            playlist.count = len(playlist.videos.all())
         return render(request, 'player/index.html',
-                      context={'playlists': Playlist.objects.filter(user=request.user)})
+                      context=context)
     return auth_views.login(request)
