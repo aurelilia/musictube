98daca7112eb56da38b03dd891005cdae921653b
Ellie Ang <git@angm.xyz>
2018-01-05 04:25:10 +0100

    Further vue progress; features completeness almost archieved

--- static/js/index.js
+++ static/js/index.js
@@ -23,44 +23,53 @@ window.onclick = function (e) {
     }
 }
 
+// Volume slider
+function updateVolume(num) {
+    if (num !== vm.volume) {
+        vm.volume = num;
+    }
+}
 
+/* VUE */
 // Get user's playlist data from the HTML the server provided
 var playlist_data = JSON.parse(document.getElementById("json").innerHTML);
-// Add playing element to the DOM, create an object for it
+// Add player object
 var player = {
-    e: document.createElement("AUDIO"),
+    e: document.getElementById("player"),
     title: "No track playing."
 };
-player.e.setAttribute("autoplay", "");
-document.body.appendChild(player.e);
 player.e.pause();
+player.e.addEventListener('ended',  function () {
+    vm.onNextTrack();
+});
 
 
 Vue.component('playlists', {
-    props: ['playlists', 'current_playlist_index'],
+    props: ['playlists'],
     template: `
-        <table width="90%">
-            <tr v-for="playlist in playlists" :key="playlist.id"
-                v-on:click="$emit('update:index', playlists.indexOf(playlist))">
-                <td class="name">{{ playlist.name }}</td>
-                <td class="context">{{ playlist.videos.length }} titles</td>
-            </tr>
-        </table>
+    <table width="90%">
+    <tr v-for="playlist in playlists" :key="playlist.id"
+    v-on:click="$emit('update:view', playlist)">
+    <td class="name">{{ playlist.name }}</td>
+    <td class="context">{{ playlist.videos.length }} titles</td>
+    </tr>
+    </table>
     `,
 });
 
 Vue.component('playlist-videos', {
-    props: ['playlists', 'current_playlist_index'],
+    props: ['playlists', 'cur_playlist_view'],
     template: `
-        <table width="90%">
-            <tr v-for="video in playlists[current_playlist_index].videos" :key="video.id"
-                v-on:click="$emit('update:track', video)">
-                <td class="name">{{ video.title }}</td>
-                <td class="context">{{ video.length }}s</td>
-            </tr>
-        </table>
+    <table width="90%">
+    <tr v-for="video in cur_playlist_view.videos" :key="video.id"
+    v-on:click="$emit('update:track', [cur_playlist_view, video])">
+    <td class="name">{{ video.title }}</td>
+    <td class="context">{{ video.length }}s</td>
+    </tr>
+    </table>
     `
 });
+
 
 var vm = new Vue({
     el: '#vue-app',
@@ -68,45 +77,68 @@ var vm = new Vue({
         playlists: playlist_data,
         player: player,
         playing: !player.e.paused,
-        current_screen: "playlists",
-        current_playlist_index: 0,
-        current_video_index: 0
+        volume: 400,
+        cur_screen: "playlists",
+        // Current playlist + video are the ones being played, cur_playlist_view is the one being
+        // looked at with the playlist-videos component
+        cur_playlist: null,
+        cur_playlist_view: null,
+        cur_video: null,
+        cur_video_index: 0
     },
+    watch: {
+        volume: function (vol) {
+            vm.player.e.volume = vol / 400;
+        },
+        cur_video_index: function (index, oldindex) {
+            if (index >= 0 && index < vm.cur_playlist.videos.length) {
 
+                if (!(vm.cur_playlist.videos[index] === vm.cur_video)) {
+                    vm.player.e.pause();
+                    vm.updateCurrentTrack(vm.cur_playlist.videos[index]);
+                }
+            } else {
+                vm.cur_video_index = 0;
+            }
+        }
+    },
     methods: {
-        onPlaylistClick(index) {
-            this.current_playlist_index = index;
-            this.current_screen = "playlist-videos";
+        onPlaylistClick(view) {
+            this.cur_playlist_view = view;
+            this.cur_screen = "playlist-videos";
         },
         updateCurrentTrack(video) {
+            vm.cur_video = video;
+            vm.cur_video_index = vm.cur_playlist.videos.indexOf(vm.cur_video);
             sendXMLHttp(null, null, "/u/" + video.url, "GET", function () {
                 if (this.readyState == 4 && this.status == 200) {
                     vm.player.title = video.title;
-                    vm.current_video_index = vm.playlists[vm.current_playlist_index].videos.indexOf(video);
                     vm.player.e.setAttribute("src", this.responseText);
                     vm.player.e.play();
                     vm.playing = !vm.player.e.paused;
                 }
             });
         },
+        updateCurrentPlaylistAndTrack(info) {
+            vm.cur_playlist = info[0];
+            vm.updateCurrentTrack(info[1]);
+        },
         onPlayPause(video) {
-            if (vm.player.e.paused && vm.player.e.src != "") {
+            if (!vm.playing && vm.player.e.src !== "") {
                 vm.player.e.play();
-            } else if (!vm.player.e.paused) {
+            } else if (vm.playing) {
                 vm.player.e.pause();
             }
             vm.playing = !vm.player.e.paused;
         },
         onPrevTrack() {
-            player.e.pause();
-            vm.current_video_index -= 1;
-            vm.updateCurrentTrack(vm.playlists[vm.current_playlist_index].videos[vm.current_video_index])
+            vm.cur_video_index -= 1;
         },
         onNextTrack() {
-            player.e.pause();
-            vm.current_video_index += 1;
-            vm.updateCurrentTrack(vm.playlists[vm.current_playlist_index].videos[vm.current_video_index])
+            vm.cur_video_index += 1;
 
         }
     }
 });
+
+updateVolume(25);
