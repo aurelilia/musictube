a6f8f9f383b450763b98f6466c35da49f8827b2d
Ellie Ang <git@angm.xyz>
2018-01-07 02:10:29 +0100

    Turns out 97c4570's function wasn't useless after all

--- static/js/player.js
+++ static/js/player.js
@@ -122,6 +122,9 @@ var vm = new Vue({
         }
     },
     methods: {
+        formatSeconds(secs) {
+            return new Date(1000 * secs).toISOString().substr(14, 5);
+        },
         onPlaylistClick(view) {
             this.cur_playlist_view = view;
             this.cur_screen = "playlist-videos";
