b447eb4fcd92bc01729799cee27dabde1180b3e3
Ellie Ang <git@angm.xyz>
2018-01-24 14:33:07 +0100

    Cover some rare edge-cases for extension autopausing logic

--- extensions/firefox/background.js
+++ extensions/firefox/background.js
@@ -1,3 +1,5 @@
+var paused_by_ext = false;
+
 function sendCommand(tabs, action) {
     for(let tab of tabs) {
         browser.tabs.sendMessage(tab.id, action);
@@ -5,15 +7,22 @@ function sendCommand(tabs, action) {
 } 
 
 function updateAudioState(ignore) {
-    action = 'play';
+    var action = 'play';
     browser.tabs.query({
         audible: true
     }).then((tabs) => {
-        for (let tab of tabs) {
-            if (!tab.url.includes('mtube.dynu.net') && tab.id !== ignore) {
+        // Ignore player tabs when looking for tabs playing audio
+        for (let tab of tabs.filter(tab => !tab.url.includes('mtube.dynu.net'))) {
+            if (tab.id !== ignore) {
                 action = 'pause';
+                paused_by_ext = true;
             }
         }
+        // The player shouldn't be resumed if the user paused it.
+        // If the extension DID pause it and wants to resume, it's no longer paused.
+        if (!paused_by_ext && action === 'play') return;
+        else if (action === 'play') paused_by_ext = false;
+        
         browser.tabs.query({
             url: '*://mtube.dynu.net/*'
         }).then((tabs) => {
@@ -21,7 +30,6 @@ function updateAudioState(ignore) {
         });
     });
 }
-
 
 browser.tabs.onRemoved.addListener(updateAudioState);
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
