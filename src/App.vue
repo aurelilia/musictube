fe09bba06c7d45b42de2baec7b213868cf22fe87
Ellie Ang <git@angm.xyz>
2018-02-07 14:22:32 +0100

    Move all SASS to vue components

--- src/App.vue
+++ src/App.vue
@@ -42,6 +42,130 @@ export default {
         Playlists,
         Videos,
         Settings
+    },
+    created () {
+        window.onunload = () => {
+            localStorage.setItem('scroll', this.$store.state.scroll_title)
+            localStorage.setItem('volume', this.$store.state.volume)
+            localStorage.setItem('random', this.$store.state.random)
+        }
+
+        window.onpopstate = this.$store.commit('updateCurrentScreen')
+
+        this.$store.commit('updateCurrentScreen')
+        this.$store.commit('loadSettings')
     }
 }
 </script>
+
+<style lang="sass">
+@import './sass/colors'
+
+body
+    margin: 0 0 5%
+    padding: 0
+    font-family: 'Open Sans', sans-serif
+    background-color: $bg
+
+.content
+    margin-top: 120px
+    margin-left: auto
+    margin-right: auto
+    width: 90%
+
+// Both Playlists and Videos component use the same styling, so it's in here.
+table
+    width: 100%
+    box-shadow: 0 5px 10px 3px $shadow
+
+tr
+    background-color: $tr
+    color: $wtext
+
+tr:hover
+    background-color: $tr-hover
+
+td
+    padding: 10px
+
+tr .name
+    width: 85%
+
+tr .delete,
+.rename
+    width: 3%
+    text-align: center
+    font-size: 1.25em
+
+// Transitions
+.component-fade-enter-active,
+.component-fade-leave-active
+  transition: opacity .3s ease
+
+.position-fade-enter-active,
+.position-fade-leave-active
+  transition: opacity .5s
+
+.component-fade-enter,
+.component-fade-leave-to,
+.position-fade-enter,
+.position-fade-leave-to
+  opacity: 0
+
+.navbar-fade-enter-active,
+.navbar-fade-leave-active
+  transition: color .3s ease
+
+.navbar-fade-enter,
+.navbar-fade-leave-to,
+.position-fade-enter,
+.position-fade-leave-to
+    color: $navbar !important
+
+// Media queries for smaller devices
+@media only screen and (max-device-width: 1024px)
+    .logo
+        display: none
+
+    .controls
+        padding-left: 15px
+
+@media only screen and (max-device-width: 768px)
+    .volume,
+    .playlist-thumb,
+    .thumb,
+    .thumb-nav
+        display: none
+
+    .track
+        overflow: hidden
+        padding: 2px 5px
+        width: 100%
+        height: 50px
+        bottom: 0
+        background-color: $navbar
+        position: fixed
+        color: $wtext
+        z-index: 100
+
+    .track-title
+        white-space: nowrap
+
+    .position-slider
+        width: 70% !important
+
+    body
+        margin-bottom: 60px
+
+    .content
+        margin-top: 85px
+
+@media only screen and (max-device-width: 400px)
+    .track-title
+        white-space: normal !important
+
+    .position-slider,
+    .position-text
+        display: none
+
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -24,3 +24,50 @@ export default {
     }
 }
 </script>
+
+<style lang="sass">
+@import '../sass/colors'
+
+nav
+    overflow: hidden
+    margin: 0
+    padding: 0
+    width: 100%
+    height: 75px
+    top: 0
+    background-color: $navbar
+    box-shadow: 0 5px 10px 3px $shadow
+    position: fixed
+    display: flex
+    justify-content: flex-start
+    align-items: center
+    color: $wtext
+    z-index: 100
+
+.logo
+   padding: 0 50px
+
+.hamburger-menu
+    font-size: 1.5em
+    padding-right: 50px
+
+.menu-dropdown
+    position: fixed
+    right: 0
+    background-color: $menu
+    min-width: 200px
+    box-shadow: 0 5px 10px 3px $shadow
+    z-index: 100
+    top: 75px
+
+a
+    color: $menu-link
+    padding: 12px 16px
+    text-decoration: none
+    display: block
+
+a:hover
+    background-color: $menu-hover
+    cursor: default
+
+</style>
